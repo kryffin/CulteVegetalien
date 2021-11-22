6e2e5210324d73c813d8930199b790808bb2251a
kryffin <nicolaskleinhentz@gmail.com>
2021-11-22 09:22:42 +0100

    Recentered every trees to the center of the Unity scene

--- Assets/Scripts/PlaceTrees.cs
+++ Assets/Scripts/PlaceTrees.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlaceTrees : MonoBehaviour
 {
 
+    private List<GameObject> _trees;
+
     public string path;
     public float spaceBetweenTrees = 1000;
     public float treeScale = 0.1f;
@@ -12,15 +14,28 @@ public class PlaceTrees : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _trees = new List<GameObject>();
+        Vector3 mean = Vector3.zero;
+
         GeoJSonParser parser = new GeoJSonParser(path);
         parser.Parse();
         foreach(TreeStruct tree in parser.trees)
         {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.parent = this.transform;
             Vector3 spherePos = new Vector3((float)tree.geometry.coordinates[0], 0, (float)tree.geometry.coordinates[1]) * spaceBetweenTrees;
             sphere.transform.position = spherePos;
             sphere.transform.localScale = Vector3.one * treeScale;
+
+            _trees.Add(sphere);
+
+            mean += spherePos;
         }
+
+        mean /= parser.trees.Count;
+
+        foreach (GameObject tree in _trees)
+            tree.transform.position = tree.transform.position - mean;
     }
 
     // Update is called once per frame
